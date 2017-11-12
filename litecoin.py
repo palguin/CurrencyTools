ccd8ead98f8500902287ec863d645864c34bd313
Pablo Alguindigue <Lito@ponderlito.attlocal.net>
2017-11-11 21:22:02 -0600

    Initial commit with code

---
+++ litecoin.py
@@ -0,0 +1,96 @@
+import sys
+import requests
+import json
+
+#Pablo Alguindigue
+'''First thing is purchase price'''
+'''Second thing is quantity of currency'''
+
+def header(x):
+	print'\n'*x
+
+def isNotEmpty(x):
+	return len(x)>0
+
+def castToFloat(x):
+	try:
+		return float(x)
+	except Exception:
+		print 'Could not cast to float'
+
+def logic(quantity, purchasePrice, currentPrice):
+	if isNotEmpty(quantity) and isNotEmpty(purchasePrice) and isNotEmpty(currentPrice):
+		purchasePriceTotal = castToFloat(quantity)* castToFloat(purchasePrice)
+		currentPriceTotal = castToFloat(quantity)*castToFloat(currentPrice)
+		gain = currentPriceTotal-purchasePriceTotal
+		header(1)
+		
+		print 'Litecoin current price: ',currentPrice
+		print 'Your purchase price: ',purchasePrice
+		
+		if gain > 0:
+			print 'Your gain: ',gain
+		else:
+			print 'Your loss: ',gain
+		header(2)
+
+	else:
+		print 'One of your variables is not set'
+
+header(3)
+purchasePrice =''
+quantity = ''
+
+if len(sys.argv)==2 and sys.argv[1] =='default':
+	try:
+		with open('/Users/Lito/Desktop/currency/default.json','r') as default:
+			default = json.load(default)
+			purchasePrice = str(default['litecoin']['price'])
+			quantity = str(default['litecoin']['amount'])
+	
+	except Exception as e:
+		print 'Could not find defaut.json'
+		sys.exit()
+
+elif len(sys.argv)==2 and not isNotEmpty(quantity):
+	purchasePrice = sys.argv[1]
+	quantity = raw_input('Enter amount of Litecoin: ')
+
+elif len(sys.argv)==3:
+	purchasePrice = sys.argv[1]
+	quantity = sys.argv[2]
+
+elif len(sys.argv)>3:
+	print 'Not sure what you\'re trying to do'
+	header()
+	sys.exit(1)
+
+else:
+	purchasePrice = raw_input('Enter Purchase Price: ')
+	quantity = raw_input('Enter amount of Litecoin: ')
+
+
+r = requests.get('https://coinmarketcap.com/currencies/litecoin/')
+transferrable = []
+
+if r.status_code ==200: 
+	for charachter in r.text[r.text.index('quote_price')+13:r.text.index('quote_price')+24]:
+		try:
+			charachter = int(charachter)
+		except Exception:
+			pass
+		if type(charachter) == int:
+			transferrable.append(charachter)
+		
+		if charachter == '.':
+			transferrable.append('.')
+	
+
+	currentPrice = str()
+	for item in transferrable:
+		currentPrice+=str(item)
+	
+	logic(quantity, purchasePrice, currentPrice)
+
+else:
+	print 'Request to website did not succeed'
